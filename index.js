23de0e432fb4084e43e8003777180fe8ba9695c2
Noah Chase <nchase@gmail.com>
2019-08-04 16:15:52 -0400

    cache-bust audio

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ import { InitSettings, GameState } from './Game';
 
 InitSettings(GameState);
 
-ReactDOM.render(<App audio={'./audio/2019-05-29-a-gift.mp3'} />, document.getElementById('app'));
+ReactDOM.render(
+  <App audio={`./audio/2019-05-29-a-gift.mp3?${+new Date()}`} />,
+  document.getElementById('app'),
+);
 
 // 1. just make it full screen
 // 2. tapping anywhere on the screen stops or starts the playback
