298a250bf5472cf432dee0cbd4b0442b9420a114
Noah Chase <nchase@gmail.com>
2019-08-05 10:45:54 -0400

    kill dead code

--- Audio.js
+++ Audio.js
@@ -34,25 +34,6 @@ export function handleAudioBackgroundGraphicsUpdate(analyserNode) {
           rotation: blinker.sprite.rotation + Math.min(avg, 0.85),
         });
       } else if (rand >= 0.96) {
-        const storedSize = {
-          x: blinker.sprite.scale.x,
-          y: blinker.sprite.scale.y,
-        };
-
-        // TweenLite.to(blinker.sprite.scale, 0.2, {
-        //   x: blinker.sprite.scale.x * 2,
-        //   y: blinker.sprite.scale.y * 2,
-        // });
-
-        // setTimeout(() => {
-        //   console.log('reverting to ', storedSize);
-
-        //   TweenLite.to(blinker.sprite.scale, 0.2, {
-        //     x: storedSize.x,
-        //     y: storedSize.y,
-        //   });
-        // }, 600);
-
         TweenLite.to(blinker.sprite, 0.2, { alpha: 1 });
       }
     });
