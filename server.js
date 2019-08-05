97af1f1a8e69d66ddd7a415f4b14cb3a1e49b3fe
Noah Chase <nchase@gmail.com>
2019-08-05 11:21:09 -0400

    nicer dev experience for building for live versus running on dev

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const PORT = process.env.PORT || 3000;
 app.set('views', process.cwd());
 app.engine('html', require('ejs').renderFile);
 
-app.use(express.static(__dirname));
-
 app.get('/', (req, res) => {
-  res.render('index.html');
+  res.render('index.html', {
+    process: { env: { ...process.env } },
+  });
 });
+
+app.use(express.static(__dirname));
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
 app.use(function(req, res, next) {
-  console.log('nope');
   res.status(404).send("Sorry can't find that!");
 });
